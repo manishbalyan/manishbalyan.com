b93433d35922da3e4337ab5cbff5c2c45c7dbd91
manishbalyan <balyan05.manish@gmail.com>
2017-09-19 22:53:07 +0530

    front page updated

--- js/main.js
+++ js/main.js
@@ -9,18 +9,6 @@
   });
 
 
-  /*----------------------------------|| On Scroll Method ||----------------------------------------------*/
-
-  $(window).scroll(function () {
-    var $scroll_pos = 0;
-    $scroll_pos = $(window).scrollTop();
-    if ($scroll_pos >= 30) {
-      $('#navigation').addClass('color_navigation');
-    } else {
-      $('#navigation').removeClass('color_navigation');
-    }
-  });
-
 
 })(jQuery);
 
